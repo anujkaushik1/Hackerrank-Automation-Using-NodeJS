a4e135ded8b9551630704f3991da9869f466e05e
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-10-11 18:46:13 +0530

    Opening Hackerrank auth page

--- hk.js
+++ hk.js
@@ -1,8 +1,19 @@
 const puppeteer = require("puppeteer");
 
 let browserOpen = puppeteer.launch({headless: false, defaultViewport: null, args: ['--start-maximized']});
+const loginLink = "https://www.hackerrank.com/auth/login";
+const email = "gixig48949@inkmoto.com";
+const password = "anujk2000";
+
+let cpage;
 
 browserOpen.then(function(browserObj){
     let browserOpenPromise = browserObj.newPage();
     return browserOpenPromise;
+
+}).then(function(newPage){
+    cpage = newPage;   
+    let goToPromise = cpage.goto(loginLink); 
+    return goToPromise;
+
 })
